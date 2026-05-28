4626d157caa8d50dda3aa8c3ed155204eca66e4f
mferemp <mferemp@gmail.com>
2026-05-27 18:29:44 -0700

    Add ERC777Gv2 with contextual device and velocity authorization

---
+++ src/ERC777Gv2.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+/**
+ * @title  ERC777Gv2
+ * ... (rest of v2 contract, as in contextual security doc)
+ */
+contract ERC777Gv2 {
+    // ... (contract code from earlier extract)
+}
